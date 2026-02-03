f3c7e7bc721ecbe9c4f7140cf786c8d677d07799
marcelkleinnr <marcelklein88@web.de>
2026-02-03 20:43:11 +0100

    init: Solution set up

---
+++ TributeBandManager/Components/Models/EventLocation.cs
@@ -0,0 +1,10 @@
+﻿namespace TributeBandManager.Components.Models
+{
+    public class EventLocation
+    {
+        public string? Name { get; set; }
+        public string? City { get; set; }       
+        public int Distance { get; set; }
+        public string? Region { get; set; }
+    }
+}
