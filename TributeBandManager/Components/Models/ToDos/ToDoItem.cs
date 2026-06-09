4757f23654fb733c7303792a3da4741b8a9def2f
marcelkleinnr <marcelklein88@web.de>
2026-06-09 09:33:30 +0200

    feat: completing todo items and interactivity for disabling the input field added, sorting of todos, refactoring

--- TributeBandManager/Components/Models/ToDos/ToDoItem.cs
+++ TributeBandManager/Components/Models/ToDos/ToDoItem.cs
@@ -1,10 +1,10 @@
 ﻿namespace TributeBandManager.Components.Models.ToDos
 {
-    public class ToDo
+    public class ToDoItem
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public bool Completed { get; set; }
-        public DateTime? TimeStampCompleted { get; set; }
+        public DateTime TimeStampCompleted { get; set; }
     }
 }
---
+++ TributeBandManager/Components/Models/ToDos/ToDoItemRepository.cs
@@ -0,0 +1,46 @@
+﻿namespace TributeBandManager.Components.Models.ToDos
+{
+    public static class ToDoItemRepository
+    {
+        private static List<ToDoItem> ToDos { get; set; } = new List<ToDoItem>
+        {
+            new ToDoItem
+            {
+                Id = 1,
+                Name = "Item 1",
+                Completed = false
+            },
+            new ToDoItem
+            {
+                Id = 2,
+                Name = "Item 2",
+                Completed = false
+            },
+            new ToDoItem
+            {
+                Id = 3,
+                Name = "Item 3",
+                Completed = false
+            },
+            new ToDoItem
+            {
+                Id = 4,
+                Name = "Item 4",
+                Completed = false
+            }
+        };
+
+        public static List<ToDoItem> GetToDos()
+        {
+            return ToDos.OrderBy(tasks => tasks.Completed).ThenByDescending(tasks => tasks.Id).ToList();
+        }
+
+        public static void AddToDo(ToDoItem toDo)
+        {
+            int maxId = ToDos.Any() ? ToDos.Max(td => td.Id) : 0;
+            toDo.Id = maxId + 1;
+            toDo.Name = "New Task";
+            ToDos.Add(toDo);
+        }
+    }
+}
